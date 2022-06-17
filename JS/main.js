50ed027dbefdc358384e84cee9be7b000039d52d
Alejandra <alatorracap@gmail.com>
2022-06-17 23:24:35 +0200

    pequeños cambios

--- JS/main.js
+++ JS/main.js
@@ -3,7 +3,9 @@ myHeaders.append("Content-type", "application/x-www-form-urlencoded");
 
 var urlencoded = new URLSearchParams();
 urlencoded.append("grant_type", "client_credentials");
-urlencoded.append("client_id", "RZmM20pVrP5mwD6DA52ebS0JHoM2aZ6I");
+
+todo: ocultar los tokens, id y secret
+rlencoded.append("client_id", "RZmM20pVrP5mwD6DA52ebS0JHoM2aZ6I");
 urlencoded.append("client_secret", "hyezkVaUsomG0Usw");
 
 var requestOptions = {
