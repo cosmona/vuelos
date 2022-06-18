32850c284956f22f939309541ab7636320e9dc1f
Alejandra <alatorracap@gmail.com>
2022-06-18 20:00:22 +0200

    get token funcional de version prueba de iñaki

--- JS/main.js
+++ JS/main.js
@@ -1,83 +1,68 @@
-"using strict";
-/* var myHeaders = new Headers();
-myHeaders.append("Content-type", "application/x-www-form-urlencoded");
+function getToken() {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-type", "application/x-www-form-urlencoded");
 
-var urlencoded = new URLSearchParams();
-urlencoded.append("grant_type", "client_credentials");
+  var urlencoded = new URLSearchParams();
+  urlencoded.append("grant_type", "client_credentials");
+  urlencoded.append("client_id", "RZmM20pVrP5mwD6DA52ebS0JHoM2aZ6I");
+  urlencoded.append("client_secret", "hyezkVaUsomG0Usw");
 
-todo: ocultar los tokens, id y secret
-rlencoded.append("client_id", "RZmM20pVrP5mwD6DA52ebS0JHoM2aZ6I");
-urlencoded.append("client_secret", "hyezkVaUsomG0Usw");
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: "follow",
+  };
 
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: urlencoded,
-  redirect: 'follow'
-};
+  return fetch(
+    "https://test.api.amadeus.com/v1/security/oauth2/token",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => JSON.parse(result).access_token)
+    .catch((error) => console.log("error", error));
+}
 
-fetch("https://test.api.amadeus.com/v1/security/oauth2/token", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-   */
-
-/* 
-  ?
-  ? Inicializacion de variables
-  ?
-  ? */
-/*
- *  originLocation-> origen del vuelo
- *  destinationLocation-> destino del vuelo
- *  deptDate-> fecha de salida
- *  numAdultos-> numero de personas que vuelan, al principio solo 1
- *  maxFlights-> numero de vuelos que se obtendran con el API, se puede obtener 250
- *  flightClass-> Economy porque no hay plata
- *
- * esto luego de los ejemplos agarrala la info de el form html
- */
-let originLocation = "SYD";
-let destinationLocation = "BKK";
-let deptDate = "2022-11-01";
-let numAdultos = 1;
-let maxFlights = 1;
-let flightClass = "ECONOMY";
-/*
- *  principio de el enlace que luego se va a llenar de info
- *  los campos estan separados por un &
- */
-let enlace = `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${originLocation}&destinationLocationCode=${destinationLocation}&departureDate=${deptDate}&adults=${numAdultos}&travelClass=ECONOMY&max=${maxFlights}`;
-
-console.log(enlace); //debbug
-
-function vuelosDisponibles(link) {
+function vuelosDisponibles(link, accessToken) {
   let myHeaders = new Headers();
-  myHeaders.append("Authorization", "Bearer eMDKatLQxfKQpRet0JLHHkUYanEA");
 
-  let urlencoded = new URLSearchParams();
+  myHeaders.append("Authorization", `Bearer ${accessToken}`);
 
   let requestOptions = {
     method: "GET",
     headers: myHeaders,
     redirect: "follow",
   };
-  let trying = {};
 
-  trying = fetch(link, requestOptions)
+  let vuelos = fetch(link, requestOptions)
     .then((response) => response.text())
     .then((result) => {
-      //console.log(typeof result, result);
       result = JSON.parse(result);
-      // console.log(typeof result, result);
       return result;
     })
     .catch((error) => console.log("error", error));
-
-  //debbug
-  console.log(trying);
-  console.log("Llamando trying");
-  return trying;
+  return vuelos;
 }
 
-vuelosDisponibles(enlace);
+/*  originLocation-> origen del vuelo
+    destinationLocation-> destino del vuelo
+    deptDate-> fecha de salida
+    numAdultos-> numero de personas que vuelan, al principio solo 1
+    maxFlights-> numero de vuelos que se obtendran con el API, se puede obtener 250
+    flightClass-> Economy porque no hay plata
+    esto luego de los ejemplos agarrala la info de el form html */
+
+let originLocation = "SYD";
+let destinationLocation = "BKK";
+let deptDate = "2022-11-01";
+let numAdultos = 1;
+let maxFlights = 1;
+let flightClass = "ECONOMY";
+let enlace = `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${originLocation}&destinationLocationCode=${destinationLocation}&departureDate=${deptDate}&adults=${numAdultos}&travelClass=ECONOMY&max=${maxFlights}`;
+
+getToken().then((res) => {
+  console.log("res", res);
+  vuelosDisponibles(enlace, res).then((result) => console.log(result));
+});
+
+//console.log('getToken();', getToken());
