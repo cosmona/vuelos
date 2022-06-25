179ca07ed100d0531e023e9fcef0912726e699ca
Inakipf <inakipf@gmail.com>
2022-06-25 17:24:00 +0200

    5 tarjetas

--- JS/Modulos/inputs.js
+++ JS/Modulos/inputs.js
@@ -11,7 +11,7 @@ async function gestionInputs(origen, destino, token) {
 
   let originLocation = origen;
   let destinationLocation = destino;
-  let maxFlights = 10;
+  let maxFlights = 5;
   let enlace = `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${originLocation}&destinationLocationCode=${destinationLocation}&departureDate=${fecha.value}&adults=${numAdultos.value}&max=${maxFlights}`;
 
   //* muestra loading
