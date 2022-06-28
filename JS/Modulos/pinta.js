7c890d6eb6236416f4ecfceb2e85f6c1f4edf077
Alejandra <alatorracap@gmail.com>
2022-06-28 12:00:25 +0200

    cambios minimos

--- JS/Modulos/pinta.js
+++ JS/Modulos/pinta.js
@@ -4,16 +4,16 @@ import { vuelosDisponibles, tellAirports } from "./api.js";
 import { gestionInputs } from "./inputs.js";
 import { loadingState } from "./carga.js";
 
-//& Pinta un vuelo
+//&+ Pinta un vuelo
 async function pintaVuelo(vuelo, access_token) {
+  console.log("vuelo", vuelo);
   let domElement = document.querySelector(".vuelos"); //~DOM - const?
   const newArticle = document.createElement("article"); //~DOM
-  
-/* 
-  //* pedimos token
-  let token = await getToken(); //TODO CORRECTO PEDIR TOKEN HAY QUE PASARLO A PINTAVUELO */
 
-  //* si el listado de vuelos esta vacio busca si ya hay una tarjeta con error en pantalla
+  /*//* pedimos token
+  let token = await getToken(); //TODO: INCORRECTO PEDIR TOKEN HAY QUE PASARLO A PINTAVUELO */
+
+  //* si el listado de vuelos está vacio busca si ya hay una tarjeta con error en pantalla
   //* si no hay tarjeta de error en pantalla pinta una añade clase error y la inserta
   if (!vuelo) {
     let errorClase = document.getElementsByClassName("error"); //~DOM
@@ -22,9 +22,9 @@ async function pintaVuelo(vuelo, access_token) {
       newArticle.classList.add("error");
       domElement.appendChild(newArticle);
       newArticle.innerHTML = `<p>No hay vuelos</p>
-      <p><img src="https://www.dictionary.com/e/wp-content/uploads/2018/03/its-a-trap.gif"></p>`;  //TODO pasar la imagen a IMG
+      <p><img src="./IMG/its-a-trap.gif"></p>`; //!Its a trap!!
     }
-  
+
     //* si el listado no esta vacio pinta la tarjeta de vuelo
   } else {
     //* valores de la tarjeta para pintar
@@ -39,7 +39,7 @@ async function pintaVuelo(vuelo, access_token) {
 
     //* si escala es = 0 pone "Directo" en DOM
     escalas === 0 ? (escalas = "Directo") : escalas;
-    
+
     //* slice corta hora de salida y llegada
     salida = salida.slice(-8, -3);
     llegada = llegada.slice(-8, -3);
@@ -60,15 +60,19 @@ async function pintaVuelo(vuelo, access_token) {
     <p>Duracion</p>
     <p>Precio</p> </article>`;
 
-    //* pinta el logo 
-    newArticle.innerHTML += `<article id="cabeceraVuelo${idVuelo}"><p><img class="logo" src="https://daisycon.io/images/airline/?width=300&height=150&color=ffffff&iata=${aerolinea}" alt=""></p><p> ${salida} - ${llegada}</p><p>${escalas}</p><p>${duracion}</p><p>${precio} € </p> </article>`; //! DOC 
+    //* pinta el logo
+    //! DOC api logos aerolines
+    newArticle.innerHTML += `
+    <article id="cabeceraVuelo${idVuelo}">
+    <p
+    ><img class="logo" src="https://daisycon.io/images/airline/?width=300&height=150&color=ffffff&iata=${aerolinea}" alt="">
+    </p>
+    <p> ${salida} - ${llegada}</p><p>${escalas}</p><p>${duracion}</p><p>${precio} € </p>
+    </article>`;
 
-    
-    //* por cada escala (segementos) prepara el section y guarda el codigo de aerolinea e iataCode
+    //*+ por cada escala (segementos) prepara el section y guarda el codigo de aerolinea e iataCode
     for (let i = 0; i < segmentos.length; i++) {
       let detalleSect = document.createElement("section");
-      let aerolinea = segmentos[i].carrierCode; //! NO LO USA?? DUPLICADO LINEA 27? YA NO LO PINTAMOS EN DETALLES?
-      let iata = segmentos[i].arrival.iataCode;
 
       //* informacion de salida
       let sal = segmentos[i].departure;
@@ -103,12 +107,12 @@ async function pintaVuelo(vuelo, access_token) {
 
       //* escucha click en cada (o solo una?) tarjeta del vuelo para plegar/desplegar escalas
       let pescadorVuelo = document.querySelector(`#cabeceraVuelo${idVuelo}`);
-      pescadorVuelo.addEventListener("click", (e) => { 
+      pescadorVuelo.addEventListener("click", (e) => {
         //* pesca los detalles del vuelo
         let pescadorDetalle = document.querySelectorAll(
           `#cabeceraVuelo${idVuelo} ~ .detalle`
         );
-        
+
         //* oculta/muestra cada detalle del vuelo
         pescadorDetalle.forEach((x) => {
           if (!x.classList.contains("notHide")) {
@@ -119,64 +123,68 @@ async function pintaVuelo(vuelo, access_token) {
         });
         e.stopImmediatePropagation();
       });
-
-      
-
-      
     }
     //~ COVID
-      //^callback (funcion de flecha)=> consulta covid
-      const datosCovid = async (access_token, aeropuertoParam) => {
+    //^+ callback (funcion de flecha)=> consulta covid
+    const datosCovid = async (access_token, aeropuertoParam) => {
+      console.log("aeropuertoParam", aeropuertoParam);
 
-        //* fech covid
-        let myHeaders = new Headers();
-        myHeaders.append("Content-type", "application/x-www-form-urlencoded");
-        myHeaders.append("Authorization", `Bearer ${access_token}`);
-        let response;
+      //* fech covid
+      let myHeaders = new Headers();
+      myHeaders.append("Content-type", "application/x-www-form-urlencoded");
+      myHeaders.append("Authorization", `Bearer ${access_token}`);
+      let response;
 
-        let requestOptions = {
-          method: "GET",
-          headers: myHeaders,
-          redirect: "follow",
-        };
-        try {
-        response = await fetch("https://62b432cca36f3a973d2e4b86.mockapi.io/api/v1/country",requestOptions); //!mockupapi.io! 
-        /*   response = await fetch(
+      let requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      }; //!DOC API
+      try {
+        response = await fetch(
+          "https://62b432cca36f3a973d2e4b86.mockapi.io/api/v1/country",
+          requestOptions
+        ); //!mockupapi.io!
+        /*  response = await fetch(
             `https://test.api.amadeus.com/v1/duty-of-care/diseases/covid19-area-report?countryCode=${aeropuertoParam[0].data[0].address.countryCode}&cityCode=${aeropuertoParam[0].data[0].address.cityCode}`,requestOptions); 
-           */ 
-          } catch (error) {
-            console.error("COVID ERROR: ", error);
-          } 
-          console.log(`response${idVuelo}`, response)
-         /*  console.log('await response.text()', await response.text()) */
-          const result_1 = await response.text();
-          //* Return datos covid parseados
-          return JSON.parse(result_1);
-        };
-        
-    //!---------------------------------------------------------------------------------------
-      //* pide datos del aeropuerto pasandole el codido iata
-      let aeropuerto = await tellAirports(vuelo.itineraries[0].segments[escalas].arrival.iataCode, access_token);
-      console.log(`aeropuerto${idVuelo}`, aeropuerto)
-   
+             */
+      } catch (error) {
+        console.error("COVID ERROR: ", error);
+      }
+      console.log(`response${idVuelo}`, response);
+      /*  console.log('await response.text()', await response.text()) */
+      const result_1 = await response.text();
+      //* Return datos covid parseados
+      return JSON.parse(result_1);
+    };
 
-      //* prepara el seccion de covid
-      //* llama a la funcion callback pasandole token y el objeto aeropuerto
-      let detalleSect2 = document.createElement("section");
-      if (!aeropuerto[0].error) {
-        console.log('cojones')
-        let covid = await datosCovid(access_token, aeropuerto);
-        console.log(`covid${idVuelo}`, covid)
-        let covidRest = covid.data.areaAccessRestriction;
+    //* pide datos del aeropuerto pasandole el codido iata
+    console.log("UMPALUMPA");
+    console.log("escalas", escalas);
+    if (escalas === "Directo") {
+      escalas = 0;
+    }
+    let aeropuerto = await tellAirports(
+      vuelo.itineraries[0].segments[escalas].arrival.iataCode,
+      access_token
+    );
+    console.log(`aeropuerto${idVuelo}`, aeropuerto);
 
+    //* prepara la seccion de covid
+    //*+ llama a la funcion callback pasandole token y el objeto aeropuerto
+    let detalleSect2 = document.createElement("section");
+    console.log("aeropuerto", aeropuerto);
+    if (!aeropuerto[0].error) {
+      let covid = await datosCovid(access_token, aeropuerto);
+      let covidRest = covid.data.areaAccessRestriction;
 
-        //* añade a el vuelo la seccion de covid y mete los datos
-        newArticle.appendChild(detalleSect2);
-        detalleSect2.classList.add("covidCard");
-        detalleSect2.innerHTML += `
+      //* añade a el vuelo la seccion de covid y mete los datos
+      newArticle.appendChild(detalleSect2);
+      detalleSect2.classList.add("covidCard");
+      detalleSect2.innerHTML += `
         <section id="cabeceraCovid${idVuelo}" class="">
           <p>
-            <img class="logo" src="https://www.chg.org.uk/wp-content/uploads/2020/03/Website-covid-icons-01.png" alt="">
+            <img class="logo" src="./IMG/covid_icon.png" alt="">
           </p>
           <p>${covid.data.area.name}</p>
         </section>
@@ -202,18 +210,18 @@ async function pintaVuelo(vuelo, access_token) {
             </details>
           </section>`;
 
-        //* escucha click en cada (o solo una?) tarjeta del vuelo para plegar/desplegar restricciones covid
+      //* tarjeta del vuelo para plegar/desplegar restricciones covid
       /*   detalleSect2.addEventListener("click", (e) => {
           let pescador = document.querySelector(`#${e.target.id} ~ section:first-child`); //~DOM
           console.log('pescador', pescador)
           pescador.classList.toggle("notHide");
         }); */
-      }
-      //~ FIN COVID
+    }
+    //~ FIN COVID
   }
-};
+}
 
-//& Muestra sugerencias origen/destino [lugar - true:origen false: destino]
+//&+ Muestra sugerencias origen/destino [lugar - true:origen false: destino]
 function showSuggestions(list, lugar) {
   //* si no se inicializa a string vacio en las predicciones pone un undefined
   let listData = "";
@@ -243,10 +251,35 @@ function showSuggestions(list, lugar) {
     } else {
       suggBoxDESTINO.innerHTML = listData;
     }
-    //*
+    //* desactiva el escuchador si es de error
     const oops = document.getElementById("oops");
     oops.removeEventListener("click", handleClickLiORIGEN);
   }
+
+  let liOrigen = suggBoxORIGEN.getElementsByTagName("li");
+  let liDestino = suggBoxDESTINO.getElementsByTagName("li");
+
+  //*+ quita la classe "active" al pasar el raton por otro li
+  suggBoxORIGEN.addEventListener("mouseover", (e) => {
+    for (let index = 0; index < liOrigen.length; index++) {
+      liOrigen[index].classList.forEach((element) => {
+        if (element === "active") {
+          liOrigen[index].classList.remove("active");
+        }
+      });
+    }
+  });
+
+  //*+ quita la classe "active" al pasar el raton por otro li
+  suggBoxDESTINO.addEventListener("mouseover", (e) => {
+    for (let index = 0; index < liDestino.length; index++) {
+      liDestino[index].classList.forEach((element) => {
+        if (element === "active") {
+          liDestino[index].classList.remove("active");
+        }
+      });
+    }
+  });
 }
 
 export { pintaVuelo, showSuggestions };
